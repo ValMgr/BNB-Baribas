022c59f483d4a36701274124e1ef0ab45f3f7a54
Valentin Magry <valou32720@gmail.com>
2022-02-18 10:53:42 +0100

    Display user's account

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,16 +3,60 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
+
+use App\Entity\Account;
+use App\Form\AccountType;
+
 
 class AccountController extends AbstractController
 {
-    #[Route('/account', name: 'account')]
-    public function index(): Response
+    private $security;
+
+    public function __construct(Security $security)
     {
+       $this->security = $security;
+       $this->user = $this->security->getUser();
+    }
+
+    #[Route('/account', name: 'account')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+
+        $accounts = $doctrine->getRepository(Account::class)->findByUserId($this->user->getId());
+
         return $this->render('account/index.html.twig', [
-            'controller_name' => 'AccountController',
+            'accounts' => $accounts
+        ]);
+    }
+
+    #[Route('/account/create', name: 'createAccoun')]
+    public function createAccount(Request $request, EntityManagerInterface $entityManager): Response
+    {
+
+        $account = new Account();
+        $form = $this->createForm(AccountType::class, $account);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+          
+            $account->setUserId($this->user);
+            $account->setRIB($num = str_pad(mt_rand(1,99999999),8,'0',STR_PAD_LEFT));
+            $account->setAmount(0);
+            $entityManager->persist($account);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('account');
+        }
+
+        return $this->render('account/new.html.twig', [
+            'acccountCreateForm' => $form->createView(),
         ]);
     }
 }
