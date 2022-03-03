073baececfeba3f196ba7de193e84754789789f4
Valentin Magry <valou32720@gmail.com>
2022-03-03 15:45:34 +0100

    Create command to add user as admin

--- src/Command/CreateAdminCommand.php
+++ src/Command/CreateAdminCommand.php
@@ -6,34 +6,45 @@ namespace App\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
 
 class CreateAdminCommand extends Command
 {
+
+
+    public function __construct(ManagerRegistry $doctrine, EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->doctrine = $doctrine;
+        parent::__construct();
+    }
+
+
     // the name of the command (the part after "bin/console")
-    protected static $defaultName = 'app:create-user';
+    protected static $defaultName = 'app:create-admin';
 
     protected function configure(): void
     {
-        // ...
+        $this->addArgument('email', InputArgument::REQUIRED, 'The email of the user.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        // ... put here the code to create the user
+       
+        $output->writeln([
+            'Add admin',
+            '============',
+            '',
+        ]);
 
-        // this method must return an integer number with the "exit status code"
-        // of the command. You can also use these constants to make code more readable
+        $user = $this->doctrine->getRepository(User::class)->findOneBy(['email' => $input->getArgument('email')]);
+        $user->setRoles(['ROLE_ADMIN']);
+        $this->entityManager->flush();
+        $output->writeln('User '.$input->getArgument('email'). ' is now admin');
 
-        // return this if there was no problem running the command
-        // (it's equivalent to returning int(0))
         return Command::SUCCESS;
-
-        // or return this if some error happened during the execution
-        // (it's equivalent to returning int(1))
-        // return Command::FAILURE;
-
-        // or return this to indicate incorrect command usage; e.g. invalid options
-        // or missing arguments (it's equivalent to returning int(2))
-        // return Command::INVALID
     }
 }
--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -13,6 +13,8 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
         return parent::index();
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
